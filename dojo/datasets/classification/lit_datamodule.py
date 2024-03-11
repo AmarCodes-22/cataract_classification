04faa23f0c5baade0a8500cdb2406f9a04d71983
Amar <amarpratap2208@gmail.com>
2024-03-11 18:14:08 +0530

    WIP: track local file artifacts

--- dojo/datasets/classification/lit_datamodule.py
+++ dojo/datasets/classification/lit_datamodule.py
@@ -112,16 +112,10 @@ class ClassificationLitDataModule(L.LightningDataModule):
     def log_version(self, logger: L.pytorch.loggers.WandbLogger, local_dataset_dir: str, stage: Literal["fit", "test"]):
         artifact_type = "dataset"
 
-        # push dataset to s3
-        s3_uri = f"{self.hparams['s3_folder']}/{logger.experiment.project}/{artifact_type}/{stage}"
-        aws_cli_command = f"aws s3 sync {local_dataset_dir} {s3_uri}"
-        subprocess.run(aws_cli_command, shell=True, capture_output=False, text=True)
-
-        # log to wandb as reference artifact
         # todo: add metadata to artifact
         artifact = wandb.Artifact(f"dataset-{stage}", type=artifact_type)
 
         # todo: using checksum=True takes too much time. can i use s3 bucket's metadata instead? (e.g. last modified date, size, etc.)
-        artifact.add_reference(s3_uri, checksum=False)
+        artifact.add_reference(f"file://{local_dataset_dir}", checksum=True)
 
         logger.use_artifact(artifact)
--- dojo/tasks/classification.py
+++ dojo/tasks/classification.py
@@ -2,6 +2,7 @@ import os
 
 from lightning.pytorch import Trainer
 
+import wandb
 from dojo.callbacks import load_checkpoint_callbacks
 from dojo.datasets import initialize_classification_lit_datamodule
 from dojo.models import initialize_classification_lit_module
@@ -12,7 +13,21 @@ def initialize_modules(cfg):
     logger = initialize_wandb_logger(**cfg.logger)
 
     exp_dir = get_exp_dir(logger)
-    resume_ckpt_fpath = os.path.join(exp_dir, "fit", f"epoch_{cfg.resume_epoch}.ckpt") if cfg.resume else None
+    if cfg.resume:
+        if cfg.resume_ckpt_fpath:
+            resume_ckpt_fpath = str(cfg.resume_ckpt_fpath)
+        else:
+            resume_ckpt_fpath = os.path.join(exp_dir, "fit", f"epoch_{cfg.resume_epoch}.ckpt") if cfg.resume else None
+        resume_ckpt_fpath = os.path.abspath(resume_ckpt_fpath)
+    else:
+        resume_ckpt_fpath = None
+
+    if resume_ckpt_fpath is not None:
+        artifact = wandb.Artifact(f"model-resume", type="model")
+
+        artifact.add_reference(f"file://{resume_ckpt_fpath}", checksum=True)
+
+        logger.use_artifact(artifact)
 
     model = initialize_classification_lit_module(resume_ckpt_fpath, **cfg.model)
     dataset = initialize_classification_lit_datamodule(**cfg.dataset)
